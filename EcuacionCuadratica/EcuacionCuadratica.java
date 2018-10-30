92c1057c2491faf0ae9de8e9f95f880525a3b58a
Javier2010204 <2010204javiergomez@gmail.com>
2018-10-29 18:34:17 -0600

    Solucion ejercicio14 mejorada

--- EcuacionCuadratica/EcuacionCuadratica.java
+++ EcuacionCuadratica/EcuacionCuadratica.java
@@ -15,23 +15,20 @@ public class EcuacionCuadratica{
 		System.out.println("Ingrese el valor de c: ");
 		c = sc.nextDouble();
 
-		double cuadrado = Math.pow(b, 2);
-		double cuatroAc = 4*a*c;
-		double discriminante = cuadrado - cuatroAc;
-		if(discriminante < 0){
-			System.out.println("Operacion no valida");
-		}else{
+		double discriminante = Math.pow(b, 2)-(4*a*c);
+		if(discriminante > 0){
 			//System.out.println(multiplicacionAC);
-			double raiz = Math.sqrt(multiplicacionAC);
+			double raiz = Math.sqrt(discriminante);
 			//System.out.println(raiz);
 			double divisor = 2*a;
-			double negativo = -b;
 			//System.out.println(divisor);
 			//System.out.println(negativo);
-			double X1 = (negativo + raiz)/divisor;
-			double X2 = (negativo - raiz)/divisor;
+			double X1 = ((b*-1)+raiz)/(2*a);
+			double X2 = ((b*-1)-raiz)/(2*a);
 			System.out.println(X1);
 			System.out.println(X2);
+		}else{
+			System.out.println("Operacion no valida");
 		}
 
 	}
