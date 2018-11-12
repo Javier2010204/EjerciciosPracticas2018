1f284a38bffd16409319889730de4d60d32897fd
Javier2010204 <2010204javiergomez@gmail.com>
2018-11-11 21:26:14 -0600

    POO Constructor con multiples atributos

--- POO/Ejercicio1/MainClass.java
+++ POO/Ejercicio1/MainClass.java
@@ -4,25 +4,64 @@ import java.util.ArrayList;
 public class MainClass{
 	public static void main(String args[]){
 
+		// int id;
+		// String nombre;
+		// int edad;
+		// String trabajo;
+		// String correo;
+
 		Scanner scanner = new Scanner(System.in);
 		ArrayList<Persona> usuarios = new ArrayList<Persona>();
+		int id;
 		String nombre;
-		String salir;
+		int edad;
+		String trabajo;
+		String correo;
+		int salir;
+
 		do{
-			Persona usuario = new Persona();
-			System.out.println("Ingrese un nombre");
-			nombre = scanner.next();
-			usuario.setNombre(nombre);
-			usuarios.add(usuario);
-			System.out.println("Si desea salir escriba salir");
-			System.out.print(">");
-			salir = scanner.next();
-		}while(!salir.equals("salir"));
-
-		for (Persona persona : usuarios) {
-			System.out.println(persona.getNombre());
-		}
-
+			System.out.println("Menu");
+			System.out.println("1. Ingresar un usuario");
+			System.out.println("2. Ver lista de usuarios");
+			System.out.println("3. Editar un usuario");
+			System.out.println("4. Eliminar un usuario");
+			System.out.println("0. Salir");
+			salir = scanner.nextInt();
+			if(salir==1){
+				Persona usuario = new Persona(0, "", 0, "", "");
+				System.out.println("Id: ");
+				id = scanner.nextInt();
+				usuario.setId(id);
+				System.out.println("Nombre: ");
+				nombre = scanner.next();
+				usuario.setNombre(nombre);
+				System.out.println("Edad: ");
+				edad = scanner.nextInt();
+				usuario.setEdad(edad);
+				System.out.println("Trabajo: ");
+				trabajo = scanner.next();
+				usuario.setTrabajo(trabajo);
+				System.out.println("Correo electronico: ");
+				correo = scanner.next();
+				usuario.setCorreo(correo);
+				usuarios.add(usuario);
+			}else if(salir==2){
+				for (Persona persona : usuarios) {
+					System.out.println("");
+					System.out.println("------------------------------------");
+					System.out.print("Id: ");
+					System.out.println(persona.getId());
+					System.out.print("Nombre: ");
+					System.out.println(persona.getNombre());
+					System.out.print("Edad: ");
+					System.out.println(persona.getEdad());
+					System.out.print("Trabajo: ");
+					System.out.println(persona.getTrabajo());
+					System.out.print("Correo: ");
+					System.out.println(persona.getCorreo());
+				}
+			}
+		}while(salir != 0);
 
 	}
 }
--- POO/Ejercicio1/Persona.java
+++ POO/Ejercicio1/Persona.java
@@ -1,6 +1,32 @@
 public class Persona{
-	
+
+	// declaracion de atributos
+
+	int id;
 	String nombre;
+	int edad;
+	String trabajo;
+	String correo;
+
+	//constructor de la clase 
+
+	public Persona(int id, String nombre, int edad, 
+		String trabajo, String correo){
+			this.id = id;
+			this.nombre = nombre;
+			this.edad = edad;
+			this.trabajo = trabajo;
+			this.correo = correo;
+	}
+
+	// getters y setters 
+	public int getId(){
+		return id;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
 
 	public String getNombre(){
 		return nombre;
@@ -8,5 +34,29 @@ public class Persona{
 
 	public void setNombre(String nombre){
 		this.nombre = nombre;
+	}
+
+	public int getEdad(){
+		return edad;
+	}
+
+	public void setEdad(int edad){
+		this.edad = edad;
+	}
+
+	public String getTrabajo(){
+		return trabajo;
+	}
+
+	public void setTrabajo(String trabajo){
+		this.trabajo = trabajo;
+	}
+
+	public String getCorreo(){
+		return correo;
+	}
+
+	public void setCorreo(String correo){
+		this.correo = correo;
 	}
 }
